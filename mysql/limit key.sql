39c53e7b12a0c279b50d95d238300bc12af9f49c
testVIjho <vithya.v@innovano.com>
2026-07-21 11:19:07 +0530

    Add SQL query for employee salary limit and offset
    Added a new query to select employees ordered by salary with limit and offset.

--- mysql/limit key.sql
+++ mysql/limit key.sql
@@ -3,3 +3,4 @@ use study;
 select*from employee;
 select *from employee limit 7,7; -- first 7 record skip pannitu next irukka 7 record return pannum
 select *from employee order by salary limit 7,7;
+-- limit la first offset,second limit
